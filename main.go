83e1d3ee3cb10a9ec304cb0491a36efa52297f38
Gennady Terekhov <ter.ge@yandex.ru>
2023-11-17 21:54:59 +0400

    develop

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
-package greetings
+package main
 
 import (
 	"fmt"
 )
 
-func Hello(name string) (string, error) {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+func main() {
+	runCalculator()
+}
 
-	return message, nil
+func runCalculator() {
+	converter := CreateConverter()
+	cliReader := CreateCliReader(&converter)
+	operandA, operandB, operation, isRoman, err := cliReader.GetInput()
+	if err != nil {
+		panic(err)
+	}
+
+	calculator := CreateCalculator()
+	answer, err := calculator.Calculate(operandA, operandB, operation)
+	if err != nil {
+		panic(err)
+	}
+
+	if !isRoman {
+		fmt.Println(answer)
+		return
+	}
+
+	if answer < 1 {
+		panic("Cannot have non-positive result when using roman numerals")
+	}
+	answerAsString, err := converter.ConvertToRoman(answer)
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Println(answerAsString)
 }
