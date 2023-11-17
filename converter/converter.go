8323bc9b19fd46d8bc9ff46460574cd94851fd50
Gennady Terekhov <ter.ge@yandex.ru>
2023-11-17 23:41:38 +0400

    add-converter

--- converter/converter.go
+++ converter/converter.go
@@ -1,11 +1,74 @@
 package converter
 
-func ConvertToRoman(arabicNumeral int) (string, error) {
+import "errors"
 
-	return "", nil
+func ConvertToRoman(arabicNumeral int) (string, error) {
+	if arabicNumeral < 1 {
+		return "", errors.New("cannot convert non-positive to roman")
+	}
+	units := arabicNumeral % 10
+	tens := arabicNumeral / 10 % 10
+	hundreds := arabicNumeral / 100 % 10
+
+	hundredsString := getHundredsString(hundreds)
+	tensString := getTensString(tens)
+	unitsString := getUnitsString(units)
+
+	return hundredsString + tensString + unitsString, nil
 }
 
-func ConvertToArabic(arabicNumeral int) (int, error) {
+var arabicToUnitsMap = [...]string{
+	"",
+	"I",
+	"II",
+	"III",
+	"IV",
+	"V",
+	"VI",
+	"VII",
+	"VIII",
+	"IX",
+}
+var arabicToTensMap = [...]string{
+	"",
+	"X",
+	"XX",
+	"XXX",
+	"XL",
+	"L",
+	"LX",
+	"LXX",
+	"LXXX",
+	"XC",
+}
 
-	return -1, nil
+// only need to support 1-10
+func ConvertToArabic(romanNumeral string) (int, error) {
+
+	if romanNumeral == "X" {
+		return 10, nil
+	}
+
+	for k, v := range arabicToUnitsMap {
+		if v == romanNumeral {
+			return k, nil
+		}
+	}
+
+	return -1, errors.New("could not convert " + romanNumeral)
+}
+
+func getHundredsString(arabicNumeral int) string {
+	if arabicNumeral == 1 {
+		return "C"
+	}
+	return ""
+}
+
+func getTensString(arabicNumeral int) string {
+	return arabicToTensMap[arabicNumeral]
+}
+
+func getUnitsString(arabicNumeral int) string {
+	return arabicToUnitsMap[arabicNumeral]
 }
